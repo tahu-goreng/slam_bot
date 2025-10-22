86bab7086830f372e25084285460c3645bc9472f
tahu_goreng <mikaelroshan@gmail.com>
2025-10-22 21:11:30 +0800

    Xacro prep

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,9 +3,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 import xacro
 
