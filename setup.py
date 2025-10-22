ea0e84abfd07e6d9bc2b629af32df99f822678e0
tahu_goreng <mikaelroshan@gmail.com>
2025-10-22 21:03:12 +0800

    Setup rviz

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+import os
+from glob import glob
+
 package_name = 'slam_bot'
 
 setup(
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'description'), glob('description/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
